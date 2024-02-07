c3c9c2e7a61235a2b629dc1d809265444128be16
AbraaoHenriqueVasconcelos2 <abraao.vscncls@gmail.com>
2024-02-07 19:52:41 -0300

    Project structure

---
+++ rest-with-spring-boot-3-1-8-and-java-21/src/main/java/br/com/example/RestWithSpringBoot318AndJava21Application.java
@@ -0,0 +1,13 @@
+package br.com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBoot318AndJava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBoot318AndJava21Application.class, args);
+	}
+
+}
